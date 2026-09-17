61eb422e620babe19659b68c5314a7f31a5605c2
Bobby Shaw <shawuk2006@gmail.com>
2026-09-17 21:57:39 +0100

    Added remove task feature

--- src/index.js
+++ src/index.js
@@ -5,18 +5,48 @@ let closeBtn = document.querySelector("#close-btn")
 let taskSection = document.querySelector("#task-render-section")
 let taskInput = document.querySelector("#task-input")
 
-let taskList= []
+let taskList = []
 
 function renderTasks() {
     const list = document.createElement("ul")
     list.setAttribute("id", "task-list")
 
-    for (const task of taskList) {
-        const item = document.createElement("li")
-        item.textContent = task.name
-        list.append(item)
+    let id = 1
+    for (const taskObject of taskList) {
+        list.append(createTask(taskObject))
+        taskObject.id = id
+        id ++
     }
     taskSection.replaceChildren(list)
+
+
+}
+
+function createTask(taskObject) {
+    const task = document.createElement("li")
+    const taskText = document.createElement("span")
+    const completeButton = document.createElement("button")
+    task.setAttribute("class", "task")
+    completeButton.type = "button"
+
+    if (taskList.length == 0) {
+        taskObject.id = 1
+    } else {
+        taskList[taskList.length-1].id = taskObject.id = taskList[taskList.length-1].id + 1
+    }
+    completeButton.setAttribute("class", "complete-btn")
+    completeButton.setAttribute("id", taskObject.id)
+    task.setAttribute("id", taskObject.id)
+
+    taskText.textContent = taskObject.name
+    task.append(completeButton, taskText)
+
+    completeButton.addEventListener("click", () => {
+        taskList.splice(taskObject.id - 1, 1)
+        renderTasks()
+    } )
+
+    return task
 }
 
 // shows dialog box
@@ -36,9 +66,7 @@ newTaskForm.addEventListener("submit", (event) => {
     const taskName = taskInput.value.trim();
     if (!taskName) return;
 
-    console.log(taskName)
-
-    taskList.push({name: taskName})
+    taskList.push({id: null, name: taskName, completed: false})
     renderTasks();
     newTaskForm.reset();
     newTaskDialog.close();
