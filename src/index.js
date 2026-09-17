6b8cbde88491fcb1a442d2168cff63e855b50486
Bobby Shaw <shawuk2006@gmail.com>
2026-09-17 12:39:57 +0100

    Added functionality for adding tasks o the list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 let newTaskButton = document.getElementById("new-task-btn")
 let newTaskForm = document.getElementById("new-task-form") 
-let popup = document.querySelector(".popup-overlay")
+let newTaskDialog = document.querySelector("#new-task-dialog")
+let closeBtn = document.querySelector("#close-btn")
+let taskSection = document.querySelector("#task-render-section")
+let taskInput = document.querySelector("#task-input")
 
-function createTask() {
+let taskList= []
 
+function renderTasks() {
+    const list = document.createElement("ul")
+    list.setAttribute("id", "task-list")
+
+    for (const task of taskList) {
+        const item = document.createElement("li")
+        item.textContent = task.name
+        list.append(item)
+    }
+    taskSection.replaceChildren(list)
 }
 
-// popup overlay
-newTaskButton.addEventListener("click", function() {
-    popup.style.display = "block"
+// shows dialog box
+newTaskButton.addEventListener("click", () => {
+    newTaskDialog.showModal();
+    taskInput.focus();
 })
+
+// closes dialog box
+closeBtn.addEventListener("click", () => {
+    newTaskDialog.close();
+})
+
+newTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskName = taskInput.value.trim();
+    if (!taskName) return;
+
+    console.log(taskName)
+
+    taskList.push({name: taskName})
+    renderTasks();
+    newTaskForm.reset();
+    newTaskDialog.close();
+})
+
+renderTasks()
